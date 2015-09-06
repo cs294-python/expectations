6fb740c2c78741d4184731e02e7c01198c93d896
Brian Hall <brian@reallymany.com>
2015-09-05 14:37:16 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# expectations.py
+
+A dead simple Python script that prints a randomly-chosen student goal from CS 296.
+
+To run:
+
+    python expectations.py
---
+++ expectations.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+student_quotes = [
+    "the basics of using Python and what it can be used for",
+    "to utilize all tools that Python has to offer to increase productivity of the configuration management department",
+    "to come out of the class feeling fairly confident about my beginner level skills",
+    "if someone gives me instructions to do something using python, I could understand those instructions and understand what my options are",
+    "gain a better appreciation for computers [so] they won’t hate me as much",
+    "to be able to know what to do, or how to find it out, without without the dread that usually comes with complex problems",
+    "my code on how to fit Gaussian’s to spectral lines to work smoothly and non-embarrassingly so that I can share it with the astronomy world and show them the power of python",
+    "mold me, shape me, into some useful member of society",
+    "anything to improve my coding skills",
+    "tools to present data",
+    "any insight you can give on simulations",
+    "explain what Brians do with Pythons.",
+    "be able to utilize vim for text editing, learn Ubuntu, make a wonderful blog.",
+    "Utilize python language to make games. That is, be able to actually make my own games without relying on existing code so much.",
+    "Learn a bit more about computers to the point that I don't feel like a complete noob. That's noob not newb. The shame.",
+    "I am somewhat interested in this 'parsing'. I am still a chemistry major so I feel that it would be important to learn how to sift through data.",
+    "Fun. There's not much time left and I would like to enjoy the hard work while lives aren't at stake. I'm a senior T.T",
+    "I am actually a pencil and paper kind of guy, but I want to be able to manipulate text as fast as the rest of the computer users.",
+    "I want to be able to customize my android. I know it's possible with a python so I must train my snake well.",
+    "to create a game (a small one) with the intention of it being educational for younger kids",
+    "to have a functioning game by the end of the semester that I can throw out onto the web for people to enjoy",
+    "focus mostly on creating something simple such as web pages or an app",
+    "learning new programming languages to have under my belt",
+    "to acquire enough knowledge to consider myself an entry level coder",
+    "make coding my new best friend",
+    "the opportunity to show practical work, maybe in the form of source code",
+    "find our own code-guru-lifestyle",
+    "others good work can be very inspirational",
+    "a course outline, hopefully with a thought behind it",
+    "take away a skill set",
+    "have an array of tools and know-how to offer a research astronomer",
+    "what is python good for?",
+    "when to use vs other language?",
+    "build a web crawler"
+]
+
+print(random.choice(student_quotes))
